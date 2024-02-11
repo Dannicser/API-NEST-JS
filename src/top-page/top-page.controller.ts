877ac0ac1e8a2751cca8c154e09e4093c9934288
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2024-02-11 17:18:38 +0300

    lesson #4.6 - create controllers

---
+++ src/top-page/top-page.controller.ts
@@ -0,0 +1,30 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { TopPageModel } from './top-page.model/top-page.model';
+import { FindTopPageDto } from './dto/find-top-page';
+
+@Controller('top-page')
+export class TopPageController {
+  @Post()
+  async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
+
+  @Get(':id')
+  async get(@Param('id') id: string) {}
+
+  @Get(':id')
+  async delete(@Param('id') id: string) {}
+
+  @Patch(':id')
+  async patch(@Param('id') id: string, @Body() dto: TopPageModel) {}
+
+  @HttpCode(200)
+  @Post()
+  async find(@Body() dto: FindTopPageDto) {}
+}
